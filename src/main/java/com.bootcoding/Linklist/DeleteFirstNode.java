bc579393d1554eff48671fb6cea134de1a0ef185
Anshul0707 <anshul.markwade786@gmail.com>
2022-12-08 15:03:43 +0530

    code to delete first Node

---
+++ src/main/java/com.bootcoding/Linklist/DeleteFirstNode.java
@@ -0,0 +1,52 @@
+package com.bootcoding.Linklist;
+
+public class DeleteFirstNode
+{
+    public static void main(String[] args)
+    {
+        Node head = createLinkedList();
+        deleteFirstNode(head);
+    }
+
+    static class Node
+    {
+        int data;
+        Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+    private static Node createLinkedList()
+    {
+        Node firstNode = new Node(10);
+        Node secondNode = new Node(20);
+        Node thirdNode = new Node(30);
+        Node forthNode = new Node(40);
+
+        Node head = firstNode;
+        head.next = secondNode;
+        secondNode.next = thirdNode;
+        thirdNode.next = forthNode;
+        forthNode.next = null;
+        return head;
+    }
+
+    public static void printLinkedList(Node head){
+        Node temp = head;
+        while (temp != null)
+        {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    private static void deleteFirstNode(Node head){
+        head = head.next;
+        printLinkedList(head);
+    }
+}
